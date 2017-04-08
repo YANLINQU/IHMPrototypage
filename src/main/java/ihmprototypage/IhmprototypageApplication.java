8945a61774d8053646d84436331bea0df7320429
YANLINQU <quyanlingg@hotmail.com>
2017-04-08 10:10:55 +0200

    first commit

---
+++ src/main/java/ihmprototypage/IhmprototypageApplication.java
@@ -0,0 +1,12 @@
+package ihmprototypage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IhmprototypageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IhmprototypageApplication.class, args);
+	}
+}
---
+++ src/test/java/ihmprototypage/IhmprototypageApplicationTests.java
@@ -0,0 +1,16 @@
+package ihmprototypage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IhmprototypageApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
